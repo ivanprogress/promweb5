d10a4e89ca255af3174f97d4410d6669d24f2702
ivanprogress <79832134+ivanprogress@users.noreply.github.com>
2021-03-29 16:07:54 +0700

    Delete tugas3_biodata.php

--- tugas3_biodata.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'tugas3_inc.php'; //mengambil data dari file lain
-echo "Nama : ".$nama; //perintah menampilan teks dan variabel
-echo ".<br> Lahir dan Tinggal : ".$alamat; //perintah menampilan teks dan variabel
-echo ".<br> Kelamin : ".$kelamin; //perintah menampilan teks dan variabel
-echo ".<br> Warga Negara : ".$negara; //perintah menampilan teks dan variabel
-echo ".<br> Pendidikan Akhir : ".$pendidikan; //perintah menampilan teks dan variabel
- ?>
